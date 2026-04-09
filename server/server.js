f2ee7a5cf1b45b926c5148b32a27e36cd7d3a687
Sachin <sachinjangid1016@gmail.com>
2026-04-10 01:13:56 +0530

    added mongoDb connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URI;
 
-app.use(express.json());
+if (!mongoUri) {
+  throw new Error("MONGO_URI is not set in the environment");
+}
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("MongoDB connected");
 
-app.listen(5000, () => console.log("Server running"));
+    app.use(express.json());
+
+    app.get("/", (req, res) => {
+      res.send("API running");
+    });
+
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
